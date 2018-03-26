bc52436a9f61395f29e93a849a5b42e0cdc9251e
javiya-rupal <rupal.javiya@gmail.com>
2018-03-26 16:58:09 +0530

    Added missing updates

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 if (!isset($_SERVER['PHP_AUTH_USER']) || ($_SERVER['PHP_AUTH_USER'] !='codeuser' ||  $_SERVER['PHP_AUTH_PW'] != 'codepass')) {  
     header('WWW-Authenticate: Basic realm="My Realm"');
     header('HTTP/1.0 401 Unauthorized');
-    //echo 'Text to send if user hits Cancel button';
     exit;
 }
 //
